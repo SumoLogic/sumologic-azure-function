2a3fa36e08f9e1384f63e63756c43e60cc89e1ff
Himanshu Pal <hp.iiita@gmail.com>
2019-10-16 01:36:50 +0530

    placeholder code added in both func

--- EventHubs/target/logs_build/EventHubs_Logs/index.js
+++ EventHubs/target/logs_build/EventHubs_Logs/index.js
@@ -6,35 +6,38 @@ var sumoHttp = require('./sumoclient');
 var dataTransformer = require('./datatransformer');
 var sumoClient;
 
+function setSourceCategory(msg, context) {
+    // msg._sumo_metadata = {
+    //     "category": "new_source_category"
+    // }
+}
 
 module.exports = function (context, eventHubMessages) {
     //var options ={ 'urlString':process.env.APPSETTING_SumoSelfEventHubBadEndpoint,'metadata':{}, 'MaxAttempts':3, 'RetryInterval':3000,'compress_data':true};
     var options ={ 'urlString':process.env.APPSETTING_SumoLogsEndpoint,'metadata':{}, 'MaxAttempts':3, 'RetryInterval':3000,'compress_data':true};
 
-
-
-
     sumoClient = new sumoHttp.SumoClient(options,context,failureHandler,successHandler);
     var transformer = new dataTransformer.Transformer();
     var messageArray = transformer.azureAudit(eventHubMessages);
     messageArray.forEach( msg => {
+        setSourceCategory(context, msg);
         sumoClient.addData(msg);
     });
     context.log(messageArray.map(function(x) { return JSON.stringify(x);}).join("\n"));
 
     function failureHandler(msgArray,ctx) {
-        ctx.log("Failed to send to Sumo, backup to storageaccount now"); 
-        if (sumoClient.messagesAttempted === sumoClient.messagesReceived) {            
-            context.bindings.outputBlob = messageArray.map(function(x) { return JSON.stringify(x);}).join("\n");            
+        ctx.log("Failed to send to Sumo, backup to storageaccount now");
+        if (sumoClient.messagesAttempted === sumoClient.messagesReceived) {
+            context.bindings.outputBlob = messageArray.map(function(x) { return JSON.stringify(x);}).join("\n");
             context.done();
         }
     }
     function successHandler(ctx) {
         ctx.log('Successfully sent to Sumo');
         if (sumoClient.messagesAttempted === sumoClient.messagesReceived) {
-            ctx.log('Sent all data to Sumo. Exit now.');        
+            ctx.log('Sent all data to Sumo. Exit now.');
             context.done();
-        }    
+        }
     }
     context.log("Flushing the rest of the buffers:");
     sumoClient.flushAll();

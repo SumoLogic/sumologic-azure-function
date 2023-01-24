e8e0b46c85c989cfa0ccb2a8eaff01208ce62600
Jayanta Kashyap <jkashyap@sumologic.com>
2023-01-24 18:20:02 +0530

    updated lastIndex func

--- AppendBlobReader/src/consumer.js
+++ AppendBlobReader/src/consumer.js
@@ -528,8 +528,8 @@ function regexIndexOf(string, regex, startpos) {
 function regexLastIndexOf(string, regex, startpos) {
     // https://stackoverflow.com/questions/19445994/javascript-string-search-for-regex-starting-at-the-end-of-the-string
     var stringToWorkWith = string.substring(startpos, string.length);
-    var match = string.match(regex);
-    return  match ? string.lastIndexOf(match.slice(-1)) : -1;
+    var match = stringToWorkWith.match(regex);
+    return  match ? stringToWorkWith.lastIndexOf(match.slice(-1)) : -1;
 }
 
 

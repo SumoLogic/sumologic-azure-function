c78cffb1438e5f55b663af1478ef582377a31c16
Priyansh Patel <prpatel.ctr@sumologic.com>
2024-02-06 15:15:28 +0530

    Unit test update

---
+++ BlockBlobReader/tests/baseblockblobtest.py
@@ -0,0 +1,47 @@
+import json
+import os
+import sys
+from sumologic import SumoLogic
+from azure.mgmt.resource import ResourceManagementClient
+
+sys.path.insert(0, '../../test_utils')
+from basetest import BaseTest
+
+
+class BaseBlockBlobTest(BaseTest):
+
+    def setUp(self):
+        self.create_credentials()
+        self.resource_client = ResourceManagementClient(self.azure_credential, self.subscription_id)
+        self.repo_name, self.branch_name = self.get_git_info()
+        self.sumologic_cli = SumoLogic(os.environ["SUMO_ACCESS_ID"], os.environ["SUMO_ACCESS_KEY"], self.api_endpoint(os.environ["SUMO_DEPLOYMENT"]))
+        self.collector_id = self.create_collector(self.collector_name)
+        self.sumo_source_id, self.sumo_endpoint_url = self.create_source(self.collector_id, self.source_name)
+        
+    def tearDown(self):
+        # if self.resource_group_exists(self.RESOURCE_GROUP_NAME):
+        #     self.delete_resource_group()
+        #     self.delete_event_subscription()
+        # self.delete_container()
+        # self.delete_source(self.collector_id, self.sumo_source_id)
+        # self.delete_collector(self.collector_id)
+        self.sumologic_cli.session.close()
+    
+    def _parse_template(self):
+        template_path = os.path.join(os.path.abspath('..'), 'src',
+                                     self.template_name)
+
+        print("Reading template from %s" % template_path)
+        with open(template_path, 'r') as template_file_fd:
+            template_data = json.load(template_file_fd)
+
+        template_data["parameters"]["SumoEndpointURL"]["defaultValue"] = self.sumo_endpoint_url
+        template_data["parameters"]["sourceCodeBranch"]["defaultValue"] = self.branch_name
+        template_data["parameters"]["sourceCodeRepositoryURL"]["defaultValue"] = self.repo_name
+        template_data["parameters"]["StorageAccountName"]["defaultValue"] = self.test_storageaccount_name
+        template_data["parameters"]["StorageAccountResourceGroupName"]["defaultValue"] = self.test_storage_res_group
+        template_data["parameters"]["StorageAccountRegion"]["defaultValue"] = self.test_storageAccountRegion
+        template_data["parameters"]["location"]["defaultValue"] = self.resourcegroup_location
+        
+
+        return template_data
--- BlockBlobReader/tests/test_blobreader.py
+++ BlockBlobReader/tests/test_blobreader.py
@@ -1,13 +1,12 @@
 import os
-import datetime
+from datetime import datetime
 import unittest
 import json
 import uuid
 from time import sleep
-import sys
+from baseblockblobtest import BaseBlockBlobTest
 from azure.storage.blob import BlockBlobService
 from azure.storage.blob.models import BlobBlock
-from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.storage import StorageManagementClient
 from azure.cosmosdb.table.tableservice import TableService
 from azure.mgmt.eventgrid import EventGridManagementClient
@@ -22,51 +21,33 @@ from azure.mgmt.eventgrid.models import (
     EventSubscription
 )
 
-
-sys.path.insert(0, '../../test_utils')
-from basetest import BaseTest
-
-
-class TestBlobReaderFlow(BaseTest):
+class TestBlobReaderFlow(BaseBlockBlobTest):
 
     def setUp(self):
-        self.create_credentials()
-        self.unique_suffix = datetime.datetime.now().strftime("%d-%m-%y-%H-%M-%S")
-        self.RESOURCE_GROUP_NAME = "TBL-%s" % (self.unique_suffix)
-
-        self.resource_client = ResourceManagementClient(self.credentials,
-                                                        self.subscription_id)
+        datetime_value = datetime.now().strftime("%d-%m-%y-%H-%M-%S")
+        self.collector_name = "azure_blob_unittest-%s" % (datetime_value)
+        self.source_name = "blob_data-%s" % (datetime_value)
+        super(TestBlobReaderFlow, self).setUp()
+        self.RESOURCE_GROUP_NAME = "TBL-%s" % (datetime_value)
         self.template_name = 'blobreaderdeploy.json'
         self.log_table_name = "AzureWebJobsHostLogs%d%02d" % (
-            datetime.datetime.now().year, datetime.datetime.now().month)
+            datetime.now().year, datetime.now().month)
         self.offsetmap_table_name = "FileOffsetMap"
 
         # self.test_storage_res_group = "ag-sumo"
         # self.test_storageaccount_name = "allbloblogs"
         self.test_storage_res_group = "SumoAuditCollection"
         self.test_storageaccount_name = "allbloblogseastus"
-        self.test_container_name = "testcontainer-%s" % (self.unique_suffix)
+        self.test_storageAccountRegion = "East US"
+        self.test_container_name = "testcontainer-%s" % (datetime_value)
         self.test_filename = "testblob"
         self.event_subscription_name = "SUMOBRSubscription"
-        try:
-            self.sumo_endpoint_url = os.environ["SumoEndpointURL"]
-        except KeyError:
-            raise Exception("SumoEndpointURL/StorageAcccountConnectionString environment variables are not set")
-
-        self.repo_name, self.branch_name = self.get_git_info()
-
-    def tearDown(self):
-        if self.resource_group_exists(self.RESOURCE_GROUP_NAME):
-            self.delete_resource_group()
-            self.delete_event_subscription()
-        self.delete_container()
 
     def test_pipeline(self):
-
         self.create_resource_group()
         self.deploy_template()
-        print("Testing Stack Creation")
         self.assertTrue(self.resource_group_exists(self.RESOURCE_GROUP_NAME))
+
         self.table_service = self.get_table_service()
         self.block_blob_service = self.get_blockblob_service(
             self.test_storage_res_group, self.test_storageaccount_name)
@@ -115,7 +96,7 @@ class TestBlobReaderFlow(BaseTest):
         return str(uuid.uuid4())
 
     def get_blockblob_service(self, resource_group, account_name):
-        storage_client = StorageManagementClient(self.credentials,
+        storage_client = StorageManagementClient(self.azure_credential,
                                                  self.subscription_id)
         storage_keys = storage_client.storage_accounts.list_keys(
             resource_group, account_name)
@@ -125,9 +106,10 @@ class TestBlobReaderFlow(BaseTest):
         return block_blob_service
 
     def get_table_service(self):
-        storage_client = StorageManagementClient(self.credentials,
+        storage_client = StorageManagementClient(self.azure_credential,
                                                  self.subscription_id)
-        STORAGE_ACCOUNT_NAME = self.get_resource_name("sumobrlogs", "Microsoft.Storage/storageAccounts")
+        STORAGE_ACCOUNT_NAME = self.get_resource_name(
+            "sumobrlogs", "Microsoft.Storage/storageAccounts")
         storage_keys = storage_client.storage_accounts.list_keys(
             self.RESOURCE_GROUP_NAME, STORAGE_ACCOUNT_NAME)
         acckey = storage_keys.keys[0].value
@@ -137,8 +119,9 @@ class TestBlobReaderFlow(BaseTest):
         return table_service
 
     def get_event_hub_resource_id(self):
-        namespace_name = self.get_resource_name("SUMOBREventHubNamespace", "Microsoft.EventHub/namespaces")
-        eventhub_client = EventHubManagementClient(self.credentials,
+        namespace_name = self.get_resource_name(
+            "SUMOBREventHubNamespace", "Microsoft.EventHub/namespaces")
+        eventhub_client = EventHubManagementClient(self.azure_credential,
                                                    self.subscription_id)
         ehitr = eventhub_client.event_hubs.list_by_namespace(
             self.RESOURCE_GROUP_NAME, namespace_name)
@@ -159,8 +142,10 @@ class TestBlobReaderFlow(BaseTest):
             resource="https://graph.windows.net"
         )
 
-        gcli = GraphRbacManagementClient(credentials, self.config['AZURE_TENANT_ID'])
-        sp = gcli.service_principals.list(filter="displayName eq '%s'" % full_app_name)
+        gcli = GraphRbacManagementClient(
+            credentials, self.config['AZURE_TENANT_ID'])
+        sp = gcli.service_principals.list(
+            filter="displayName eq '%s'" % full_app_name)
         sp = next(sp, False)
         if sp:
             print("Found Service Principal %s" % sp.display_name)
@@ -173,9 +158,11 @@ class TestBlobReaderFlow(BaseTest):
         resource_type = "storageAccounts"
         scope = '/subscriptions/%s/resourceGroups/%s/providers/%s/%s/%s' % (
             self.subscription_id, resource_group_name, resource_provider, resource_type, storage_name)
-        auth_cli = AuthorizationManagementClient(self.credentials, self.subscription_id, api_version="2015-07-01")
+        auth_cli = AuthorizationManagementClient(
+            self.azure_credential, self.subscription_id, api_version="2015-07-01")
         resp = auth_cli.role_assignments.delete(scope, role_assignment_name)
-        print("%s App Service access revoked %s Storage account" % (role_assignment_name, storage_name))
+        print("%s App Service access revoked %s Storage account" %
+              (role_assignment_name, storage_name))
 
     def assign_keylistrole_appservice(self, resource_group_name, storage_name, app_service_name):
         resource_provider = "Microsoft.Storage"
@@ -184,13 +171,18 @@ class TestBlobReaderFlow(BaseTest):
             self.subscription_id, resource_group_name, resource_provider, resource_type, storage_name)
         role_assignment_name = str(uuid.uuid4())
         # id for "Storage Account Key Operator Service Role" https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles#storage-account-key-operator-service-role
-        role_id = "/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/%s" % (self.subscription_id, "81a9662b-bebf-436f-a333-f67b29880f12")
+        role_id = "/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/%s" % (
+            self.subscription_id, "81a9662b-bebf-436f-a333-f67b29880f12")
         principal_id = self.get_object_id(app_service_name)
-        props = RoleAssignmentProperties(role_definition_id=role_id, principal_id=principal_id)
+        props = RoleAssignmentProperties(
+            role_definition_id=role_id, principal_id=principal_id)
 
-        auth_cli = AuthorizationManagementClient(self.credentials, self.subscription_id, api_version="2015-07-01")
-        resp = auth_cli.role_assignments.create(scope, role_assignment_name, properties=props)
-        print("%s App Service authorized to access %s Storage account" % (app_service_name, storage_name))
+        auth_cli = AuthorizationManagementClient(
+            self.azure_credential, self.subscription_id, api_version="2015-07-01")
+        resp = auth_cli.role_assignments.create(
+            scope, role_assignment_name, properties=props)
+        print("%s App Service authorized to access %s Storage account" %
+              (app_service_name, storage_name))
         return role_assignment_name
 
     def create_offset_table(self):
@@ -213,32 +205,42 @@ class TestBlobReaderFlow(BaseTest):
 
     def create_event_subscription(self):
         print("creating event subscription")
-        event_client = EventGridManagementClient(self.credentials, self.subscription_id)
+        event_client = EventGridManagementClient(
+            self.azure_credential, self.subscription_id)
 
-        scope = '/subscriptions/'+self.subscription_id+'/resourceGroups/'+self.test_storage_res_group+'/providers/microsoft.storage/storageaccounts/%s' % self.test_storageaccount_name
-        destination = EventHubEventSubscriptionDestination(resource_id=self.get_event_hub_resource_id())
+        scope = '/subscriptions/'+self.subscription_id+'/resourceGroups/'+self.test_storage_res_group + \
+            '/providers/microsoft.storage/storageaccounts/%s' % self.test_storageaccount_name
+        destination = EventHubEventSubscriptionDestination(
+            resource_id=self.get_event_hub_resource_id())
         esfilter = EventSubscriptionFilter(**{
             "subject_begins_with": "/blobServices/default/containers/%s/" % self.test_container_name,
             "subject_ends_with": "",
             "is_subject_case_sensitive": False,
             "included_event_types": ["Microsoft.Storage.BlobCreated"]
         })
-        event_subscription_info = EventSubscription(destination=destination, filter=esfilter)
-        create_resp = event_client.event_subscriptions.create_or_update(scope, self.event_subscription_name, event_subscription_info)
+        event_subscription_info = EventSubscription(
+            destination=destination, filter=esfilter)
+        create_resp = event_client.event_subscriptions.create_or_update(
+            scope, self.event_subscription_name, event_subscription_info)
         create_resp.wait()
 
     def delete_event_subscription(self):
         print("deleting event subscription")
-        event_client = EventGridManagementClient(self.credentials, self.subscription_id)
-        scope = '/subscriptions/'+self.subscription_id+'/resourceGroups/'+self.test_storage_res_group+'/providers/microsoft.storage/storageaccounts/%s' % self.test_storageaccount_name
-        event_client.event_subscriptions.delete(scope, self.event_subscription_name+self.res_suffix)
+        event_client = EventGridManagementClient(
+            self.azure_credential, self.subscription_id)
+        scope = '/subscriptions/'+self.subscription_id+'/resourceGroups/'+self.test_storage_res_group + \
+            '/providers/microsoft.storage/storageaccounts/%s' % self.test_storageaccount_name
+        event_client.event_subscriptions.delete(
+            scope, self.event_subscription_name+self.res_suffix)
 
     def create_or_update_blockblob(self, container_name, file_name, datalist, blocks):
         block_id = self.get_random_name()
         file_bytes = ''.join(datalist).encode()
-        self.block_blob_service.put_block(container_name, file_name, file_bytes, block_id)
+        self.block_blob_service.put_block(
+            container_name, file_name, file_bytes, block_id)
         blocks.append(BlobBlock(id=block_id))
-        self.block_blob_service.put_block_list(container_name, file_name, blocks)
+        self.block_blob_service.put_block_list(
+            container_name, file_name, blocks)
         return blocks
 
     def get_current_blocks(self, test_container_name, test_filename):
@@ -261,22 +263,28 @@ class TestBlobReaderFlow(BaseTest):
         for file_bytes in json_data:
             file_bytes = file_bytes.encode()
             block_id = self.get_random_name()
-            self.block_blob_service.put_block(container_name, file_name, file_bytes, block_id)
+            self.block_blob_service.put_block(
+                container_name, file_name, file_bytes, block_id)
             blocks.append(BlobBlock(id=block_id))
-            self.block_blob_service.put_block_list(container_name, file_name, blocks)
+            self.block_blob_service.put_block_list(
+                container_name, file_name, blocks)
         return blocks
 
     def insert_mock_json_in_BlobStorage(self):
         test_filename = self.test_filename + ".json"
         # Todo refactor this to get current blocks
-        blocks = self.insert_empty_json(self.test_container_name, test_filename)
+        blocks = self.insert_empty_json(
+            self.test_container_name, test_filename)
         for i, data_block in enumerate(self.get_json_data()):
             block_id = self.get_random_name()
             file_bytes = json.dumps(data_block)
-            file_bytes = (file_bytes[1:-1] if i == 0 else "," + file_bytes[1:-1]).encode()
-            self.block_blob_service.put_block(self.test_container_name, test_filename, file_bytes, block_id)
+            file_bytes = (file_bytes[1:-1] if i ==
+                          0 else "," + file_bytes[1:-1]).encode()
+            self.block_blob_service.put_block(
+                self.test_container_name, test_filename, file_bytes, block_id)
             blocks.insert(len(blocks)-1, BlobBlock(id=block_id))
-            self.block_blob_service.put_block_list(self.test_container_name, test_filename, blocks)
+            self.block_blob_service.put_block_list(
+                self.test_container_name, test_filename, blocks)
         print("inserted %s" % (blocks))
 
     def get_csv_data(self):
@@ -302,9 +310,11 @@ class TestBlobReaderFlow(BaseTest):
 
     def insert_mock_logs_in_BlobStorage(self, file_ext):
         blocks = []
-        datahandler = {'log': 'get_log_data', 'csv': 'get_csv_data', 'blob': 'get_blob_formatted_data'}
+        datahandler = {'log': 'get_log_data',
+                       'csv': 'get_csv_data', 'blob': 'get_blob_formatted_data'}
         test_filename = self.test_filename + "." + file_ext
-        blocks = self.get_current_blocks(self.test_container_name, test_filename)
+        blocks = self.get_current_blocks(
+            self.test_container_name, test_filename)
         for data_block in getattr(self, datahandler.get(file_ext))():
             blocks = self.create_or_update_blockblob(self.test_container_name,
                                                      test_filename,
@@ -328,15 +338,18 @@ class TestBlobReaderFlow(BaseTest):
 
     def print_invocation_logs(self):
         max_retries = 50
-        while(max_retries > 0 and (not self.is_task_consumer_invoked())):
-            print("waiting for invocation logs...", max_retries, self.is_task_consumer_invoked())
+        while (max_retries > 0 and (not self.is_task_consumer_invoked())):
+            print("waiting for invocation logs...",
+                  max_retries, self.is_task_consumer_invoked())
             sleep(15)
             max_retries -= 1
 
-        rows = self.table_service.query_entities(self.log_table_name, filter="PartitionKey eq 'I'")
+        rows = self.table_service.query_entities(
+            self.log_table_name, filter="PartitionKey eq 'I'")
 
         for row in sorted(rows.items, key=lambda k: k['FunctionName']):
-            print(row.get("FunctionName"), str(row.get('StartTime')), str(row.get('EndTime')))
+            print(row.get("FunctionName"), str(
+                row.get('StartTime')), str(row.get('EndTime')))
             print(row.get("ErrorDetails"))
             print(row.get('LogOutput', '').encode('utf-8'))
 
@@ -359,21 +372,6 @@ class TestBlobReaderFlow(BaseTest):
 
         self.assertFalse(haserr)
 
-    def _parse_template(self):
-        template_path = os.path.join(os.path.abspath('..'), 'src',
-                                     self.template_name)
-
-        print("Reading template from %s" % template_path)
-        with open(template_path, 'r') as template_file_fd:
-            template_data = json.load(template_file_fd)
-
-        template_data["parameters"]["SumoEndpointURL"]["defaultValue"] = self.sumo_endpoint_url
-        template_data["parameters"]["sourceCodeBranch"]["defaultValue"] = self.branch_name
-        template_data["parameters"]["sourceCodeRepositoryURL"]["defaultValue"] = self.repo_name
-        template_data["parameters"]["StorageAccountName"]["defaultValue"] = self.test_storageaccount_name
-        template_data["parameters"]["StorageAccountResourceGroupName"]["defaultValue"] = self.test_storage_res_group
-
-        return template_data
 
 if __name__ == '__main__':
     unittest.main()

e701d887c8150424e3be96a5f71e59733e940fae
Priyansh Patel <prpatel.ctr@sumologic.com>
2024-02-29 03:14:24 +0530

    producer function

--- AppendBlobReader/target/producer_build/BlobTaskProducer/index.js
+++ AppendBlobReader/target/producer_build/BlobTaskProducer/index.js
@@ -1,33 +1,36 @@
-/////////////////////////////////////////////////////////////////////////////////////////////////
-//           Function to create Block Blob tasks using EventGrid Events into Azure Service Bus //
-/////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+//           Function to create tasks using EventGrid Events into Azure EventHubs               //
+///////////////////////////////////////////////////////////////////////////////////
 
-var storage = require('azure-storage');
-var tableService = storage.createTableService(process.env.APPSETTING_AzureWebJobsStorage);
+var sumoutils = require('./sumoutils.js');
+var { TableClient } = require("@azure/data-tables");
+var tableClient = TableClient.fromConnectionString(process.env.APPSETTING_AzureWebJobsStorage, process.env.APPSETTING_TABLE_NAME);
+const MaxAttempts = 3
+const RetryInterval = 3000
 
-function getRowKey(url) {
-    var storageName = url.split("//").pop().split(".")[0];
-    var arr = url.split('/').slice(3);
+function getRowKey(metadata) {
+    var storageName = metadata.url.split("//").pop().split(".")[0];
+    var arr = metadata.url.split('/').slice(3);
     var keyArr = [storageName];
     Array.prototype.push.apply(keyArr, arr);
     return keyArr.join("-");
 }
 
-function getBlobMetadata(message, contentLength) {
+function getBlobMetadata(message) {
     var url = message.data.url;
     var data = url.split('/');
     var topicArr = message.topic.split('/');
 
-    //topicArr: '/subscriptions/c088dc46-d692-42ad-a4b6-9a542d28ad2a/resourceGroups/AG-SUMO/providers/Microsoft.Storage/
-    // url: 'https://allbloblogs.blob.core.windows.net/webapplogs/AZUREAUDITEVENTHUB/2018/04/26/09/f4f692.log'
+    // '/subscriptions/c088dc46-d692-42ad-a4b6-9a542d28ad2a/resourceGroups/AG-SUMO/providers/Microsoft.Storage/
+    //'https://allbloblogs.blob.core.windows.net/webapplogs/AZUREAUDITEVENTHUB/2018/04/26/09/f4f692.log'
     return {
-        rowKey: getRowKey(url),
+        url: url,
         containerName: data[3],
         blobName: data.slice(4).join('/'),
         storageName: url.split("//").pop().split(".")[0],
         resourceGroupName: topicArr[4],
         subscriptionId: topicArr[2],
-        blobType: contentLength === 0 ? "AppendBlob" : "BlockBlob"
+        blobType: "AppendBlob"
     };
 }
 
@@ -55,25 +58,25 @@ function getBlobMetadata(message, contentLength) {
  */
 function getEntity(metadata, startByte, currentEtag) {
     //a single entity group transaction is limited to 100 entities. Also, the entire payload of the transaction may not exceed 4MB
-    var entGen = storage.TableUtilities.entityGenerator;
-    // RowKey/Partition key cannot contain "/"
+    // rowKey/partitionKey cannot contain "/"
     var entity = {
-        PartitionKey: entGen.String(metadata.containerName),
-        RowKey: entGen.String(metadata.rowKey),
-        blobName: entGen.String(metadata.blobName),
-        containerName: entGen.String(metadata.containerName),
-        storageName: entGen.String(metadata.storageName),
-        offset: entGen.Int64(startByte),
-        eventdate: entGen.DateTime((new Date()).toISOString()),
-        blobType: entGen.String(metadata.blobType),
-        done: entGen.Boolean(false),
-        resourceGroupName: entGen.String(metadata.resourceGroupName),
-        subscriptionId: entGen.String(metadata.subscriptionId)
+        partitionKey: metadata.containerName,
+        rowKey: getRowKey(metadata),
+        blobName: metadata.blobName,
+        containerName: metadata.containerName,
+        storageName: metadata.storageName,
+        offset: endByte,
+        eventdate: new Date().toISOString(),
+        blobType: metadata.blobType,
+        done: false,
+        resourceGroupName: metadata.resourceGroupName,
+        subscriptionId: metadata.subscriptionId
     };
     if (currentEtag) {
-        entity['.metadata'] = { etag: currentEtag };
+        entity['options'] = {
+            ifMatch: currentEtag, // Replace with the current ETag value of the entity
+        };
     }
-
 
     return entity;
 }
@@ -81,22 +84,21 @@ function getEntity(metadata, startByte, currentEtag) {
  * @param  {} eventHubMessages
  * @param  {} allcontentlengths
  * @param  {} metadatamap
- * @param  {} context
  * it creates a map {rowkey: [len1, len2, len3]}
  * This is done to take care of the ordering of events in a batch
  */
-function getContentLengthPerBlob(eventHubMessages, allcontentlengths, metadatamap, context) {
-
+function getContentLengthPerBlob(eventHubMessages, allcontentlengths, metadatamap) {
     eventHubMessages.forEach(function (message) {
         var contentLength = message.data.contentLength;
-        var metadata = getBlobMetadata(message, contentLength);
-        var RowKey = getRowKey(message.data.url);
-        metadatamap[RowKey] = metadata;
+        var metadata = getBlobMetadata(message);
+        var rowKey = getRowKey(metadata);
+        metadatamap[rowKey] = metadata;
         if (contentLength >= 0) {
-            (allcontentlengths[RowKey] || (allcontentlengths[RowKey] = [])).push(contentLength);
+            (allcontentlengths[rowKey] || (allcontentlengths[rowKey] = [])).push(message.data.contentLength);
         }
     });
 }
+
 /**
  * @param  {} PartitionKey
  * @param  {} RowKey
@@ -104,33 +106,37 @@ function getContentLengthPerBlob(eventHubMessages, allcontentlengths, metadatama
  *
  * retrieves the offset for a row from the table
  */
-function getBlobPointerMap(PartitionKey, RowKey, context) {
+async function getBlobPointerMap(partitionKey, rowKey, context) {
     // Todo Add retries for node migration in cases of timeouts(non 400 & 500 errors)
-    return new Promise(function (resolve, reject) {
-        tableService.retrieveEntity(process.env.APPSETTING_TABLE_NAME, PartitionKey, RowKey, function (error, result, response) {
-            context.log("inside getBlobPointerMap: ", RowKey, response.statusCode);
-            if (response.statusCode === 404 || !error) {
-                resolve(response);
-            } else {
-                reject(error);
-            }
-        });
-    });
+    var statusCode = 200;
+    try {
+        var entity = await tableClient.getEntity(partitionKey, rowKey);
+        //context.log("retreived existing rowkey: " + rowKey)
+    } catch (err) {
+        // err object keys : [ 'name', 'code', 'statusCode', 'request', 'response', 'details' ]
+        if (err.statusCode === 404) {
+            //context.log("no existing row found, new file scenario for rowkey: " + rowKey)
+            statusCode = 404;
+        } else {
+            throw err;
+        }
+    }
+    //context.log({statusCode: statusCode, entity: entity});
+    return { statusCode: statusCode, entity: entity };
 }
 
-function updateBlobPointerMap(entity, context) {
-    return new Promise(function (resolve, reject) {
-        var insertOrReplace = ".metadata" in entity ? tableService.replaceEntity.bind(tableService) : tableService.insertEntity.bind(tableService);
-        insertOrReplace(process.env.APPSETTING_TABLE_NAME, entity, function (error, result, response) {
-            // context.log("inside updateBlobPointerMap: ", response.statusCode);
-            if (!error) {
-                resolve(response);
-            } else {
-                reject(error);
-            }
-        });
-    });
+async function updateBlobPointerMap(entity, context) {
+    let response;
+    if (entity.options) {
+        let options = entity.options;
+        delete entity.options;
+        response = await tableClient.updateEntity(entity, "Replace", options)
+    } else {
+        response = await tableClient.createEntity(entity)
+    }
+    return response;
 }
+
 /**
  * @param  {} PartitionKey
  * @param  {} RowKey
@@ -144,104 +150,130 @@ function updateBlobPointerMap(entity, context) {
  *
  * In cases of Block blob a task is created in service bus and consumer function consumes the task by download the file using the offset in task metadata and then sending to sumo logic's http endpoint
  */
-function createTasksForBlob(PartitionKey, RowKey, sortedcontentlengths, context, metadata, finalcontext) {
-    // context.log("inside createTasksForBlob", PartitionKey, RowKey, sortedcontentlengths, metadata);
-    getBlobPointerMap(PartitionKey, RowKey, context).then(function (response) {
-        var tasks = [];
-        var currentoffset = response.statusCode === 404 ? -1 : Number(response.body.offset);
-        var currentEtag = response.statusCode === 404 ? null : response.body['odata.etag'];
-        var lastoffset = currentoffset;
-        var i, endByte, task;
-        for (i = 0; i < sortedcontentlengths.length; i += 1) {
-            endByte = sortedcontentlengths[i] - 1;
-            if (endByte > lastoffset) {
-                // this will remove duplicate contentlengths
-                // to specify a range encompassing the first 512 bytes of a blob use x-ms-range: bytes=0-511  contentLength = 512
-                // saving in offset: 511 endByte
-                task = Object.assign({
-                    startByte: lastoffset + 1,
-                    endByte: endByte
-                }, metadata);
-                tasks.push(task);
-                lastoffset = endByte;
+async function createTasksForBlob(partitionKey, rowKey, sortedcontentlengths, context, metadata) {
+    //context.log("inside createTasksForBlob", partitionKey, rowKey, sortedcontentlengths, metadata);
+    if (sortedcontentlengths.length === 0) {
+        return Promise.resolve({ status: "success", message: "No tasks created for rowKey: " + rowKey });
+    }
+    try {
+        var retrievedResponse = await getBlobPointerMap(partitionKey, rowKey, context);
+        //context.log("retrieved blob pointer successsfully for rowkey: " + rowKey + " response: "+ retrievedResponse)
+    } catch (err) {
+        // unable to retrieve offset, hence ingesting whole file from starting byte
+        let lastoffset = sortedcontentlengths[sortedcontentlengths.length - 1] - 1;
+        return Promise.reject({ status: "failed", rowKey: rowKey, message: "Unable to Retrieve offset for rowKey: " + rowKey + " Error: " + err, lastoffset: lastoffset, currentoffset: -1 });
+    }
+    var currentoffset = retrievedResponse.statusCode === 404 ? -1 : Number(retrievedResponse.entity.offset);
+    var currentEtag = retrievedResponse.statusCode === 404 ? null : retrievedResponse.entity.etag;
+    var [tasks, lastoffset] = getNewTask(currentoffset, sortedcontentlengths, metadata);
+
+    if (tasks.length > 0) { // modify offset only when it's been changed
+        var entity = getEntity(metadata, lastoffset, currentEtag);
+        try {
+            var updatedResponse = await updateBlobPointerMap(entity, context);
+            //context.log("updated blob pointer successsfully for rowkey: " + rowKey + " response: "+ updatedResponse)
+            context.bindings.tasks = context.bindings.tasks.concat(tasks);
+            return Promise.resolve({ status: "success", rowKey: rowKey, message: tasks.length + " Tasks added for rowKey: " + rowKey });
+        } catch (err) {
+            if (err && err.details && err.details.odataError && err.details.odataError.code === "UpdateConditionNotSatisfied" && err.statusCode === 412) {
+                context.log.verbose("Need to Retry: " + rowKey);
             }
+            return Promise.reject({ status: "failed", rowKey: rowKey, message: "Unable to Update offset for rowKey: " + rowKey + " Error: " + err, lastoffset: lastoffset, currentoffset: currentoffset });
         }
-        //Todo:  Bulk Row creation will reduce num of api calls.
-        if (lastoffset > currentoffset) { // modify offset only when it's been changed
-            context.log("Block blob scenario updating offset to: " + lastoffset + " from: " + currentoffset+ " RowKey: ", RowKey)
-            var entity = getEntity(metadata, lastoffset, currentEtag);
-            updateBlobPointerMap(entity, context).then(function (response) {
-                context.bindings.tasks = context.bindings.tasks.concat(tasks);
-                finalcontext(null, tasks.length + " Block blob Entry added for RowKey: " + RowKey);
-            }).catch(function (err) {
-                //handle catch with retry when If-match fails else other err
-                if (err.code === "UpdateConditionNotSatisfied") {
-                    context.log("Need to Retry: " + RowKey, entity);
-                }
-                finalcontext(err, "Unable to Update offset for RowKey: " + RowKey);
-
-            });
-        } else if (currentoffset === -1 && lastoffset === -1) {
-            context.log("Append blob scenario create just an entry RowKey: ", RowKey)
-            var entity = getEntity(metadata, 0, currentEtag);
-            updateBlobPointerMap(entity, context).then(function (response) {
-                finalcontext(null, "AppendBlob Entry added for RowKey: " + RowKey);
-            }).catch(function (err) {
-                //handle catch with retry when If-match fails else other err
-                if (err.code === "UpdateConditionNotSatisfied") {
-                    context.log("Need to Retry: " + RowKey, entity);
-                }
-                finalcontext(err, "Unable to Update offset for RowKey: " + RowKey);
-            });
-
-        } else {
-            finalcontext(null, "No tasks created for RowKey: " + RowKey);
-        }
-
-    }).catch(function (err) {
-        // unable to retrieve offset
-        finalcontext(err, "Unable to Retrieve offset for RowKey: " + RowKey);
-    });
-
+    } else {
+        return Promise.resolve({ status: "success", rowKey: rowKey, message: "No tasks created for rowKey: " + rowKey });
+    }
 }
 
-module.exports = function (context, eventHubMessages) {
-    context.log("eventHubMessages: ", eventHubMessages);
+
+function getNewTask(currentoffset, sortedcontentlengths, metadata) {
+    var tasks = [];
+    var lastoffset = currentoffset;
+    var i, endByte, task;
+    for (i = 0; i < sortedcontentlengths.length; i += 1) {
+        endByte = sortedcontentlengths[i] - 1;
+        if (endByte > lastoffset) {
+            // this will remove duplicate contentlengths
+            // to specify a range encompassing the first 512 bytes of a blob use x-ms-range: bytes=0-511  contentLength = 512
+            // saving in offset: 511 endByte
+            task = Object.assign({
+                startByte: lastoffset + 1,
+                endByte: endByte
+            }, metadata);
+            tasks.push(task);
+            lastoffset = endByte;
+        }
+    }
+    return [tasks, lastoffset];
+}
+
+module.exports = async function (context, eventHubMessages) {
+    // eventHubMessages = [
+    //     [
+    //         {
+    //             topic: '/subscriptions/c088dc46-d692-42ad-a4b6-9a542d28ad2a/resourceGroups/SumoAuditCollection/providers/Microsoft.Storage/storageAccounts/allbloblogseastus',
+    //             subject: '/blobServices/default/containers/testabb/blobs/dummy_data.csv',
+    //             eventType: 'Microsoft.Storage.BlobCreated',
+    //             id: '6a093faf-501e-006c-2f87-6a4998067a11',
+    //             data: {
+    //                 api: 'PutBlob',
+    //                 requestId: '075ec13d-401e-0060-0a88-6ade90000000',
+    //                 eTag: '0x8DC389F4D015905',
+    //                 contentType: 'text/csv',
+    //                 contentLength: 0,
+    //                 blobType: 'AppendBlob',
+    //                 url: 'https://allbloblogseastus.blob.core.windows.net/testabb/dummy_data.csv',
+    //                 sequencer: '00000000000000000000000000010F87000000000001955c',
+    //                 storageDiagnostics: {
+    //                     batchId: 'd7242b73-7006-0036-0088-6a2f7f000000'
+    //                 }
+    //             },
+    //             dataVersion: '',
+    //             metadataVersion: '1',
+    //             eventTime: new Date().toISOString()
+    //         }
+    //     ]
+    // ]
     try {
         eventHubMessages = [].concat.apply([], eventHubMessages);
-        context.log("blobtaskproducer message received: ", eventHubMessages.length);
         var metadatamap = {};
         var allcontentlengths = {};
-        getContentLengthPerBlob(eventHubMessages, allcontentlengths, metadatamap, context);
+        getContentLengthPerBlob(eventHubMessages, allcontentlengths, metadatamap);
         var processed = 0;
         context.bindings.tasks = [];
+        var allRowPromises = [];
         var totalRows = Object.keys(allcontentlengths).length;
-        var errArr = [], RowKey;
-        for (RowKey in allcontentlengths) {
-            var sortedcontentlengths = allcontentlengths[RowKey].sort(); // ensuring increasing order of contentlengths
-            var metadata = metadatamap[RowKey];
-            var PartitionKey = metadata.containerName;
-
-            createTasksForBlob(PartitionKey, RowKey, sortedcontentlengths, context, metadata, function (err, msg) {
+        var errArr = [], rowKey;
+        for (rowKey in allcontentlengths) {
+            var sortedcontentlengths = allcontentlengths[rowKey].sort(); // ensuring increasing order of contentlengths
+            var metadata = metadatamap[rowKey];
+            var partitionKey = metadata.containerName;
+            allRowPromises.push(sumoutils.p_retryMax(createTasksForBlob, MaxAttempts, RetryInterval, [partitionKey, rowKey, sortedcontentlengths, context, metadata]).catch((err) => err));
+        }
+        await Promise.all(allRowPromises).then((responseValues) => {
+            //creating duplicate task for file causing an error when update condition is not satisfied in mutiple read and write scenarios for same row key in fileOffSetMap table
+            for (let response of responseValues) {
                 processed += 1;
-
-                if (err) {
-                    errArr.push(err);
-                } else {
-                    context.log("Created Entry in Offset Table: ", RowKey, processed, err, msg);
+                if (response.status === "failed") {
+                    context.log.verbose("creating duplicate task since retry failed for rowkey: " + response.rowKey);
+                    var duplicateTask = Object.assign({
+                        startByte: response.currentoffset + 1,
+                        endByte: response.lastoffset
+                    }, metadatamap[response.rowKey]);
+                    context.bindings.tasks = context.bindings.tasks.concat([duplicateTask]);
+                    errArr.push(response.message);
                 }
-                if (totalRows === processed) {
-                    context.log("ALL Events Processed: " + context.bindings.tasks.length + " Blobpaths: " + JSON.stringify(allcontentlengths));
-                    if (errArr.length > 0) {
-                        context.done(errArr.join('\n'));
-                    } else {
-                        context.done();
-                    }
-                }
-            });
-
+            }
+        });
+        if (totalRows === processed) {
+            context.log("Tasks Created: " + JSON.stringify(context.bindings.tasks) + " Blobpaths: " + JSON.stringify(allcontentlengths));
+            if (errArr.length > 0) {
+                context.log.error(errArr.join('\n'));
+            }
+            context.done();
         }
     } catch (error) {
+        context.log.error(error)
         context.done(error);
     }
 };

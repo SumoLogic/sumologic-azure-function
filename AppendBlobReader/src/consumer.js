d81ea5cf2605d94e6408d8b54b81a498d1f0daa5
Priyansh Patel <prpatel.ctr@sumologic.com>
2024-03-14 14:02:58 +0530

    SUMO-236064: Update Consumer Logs

--- AppendBlobReader/src/consumer.js
+++ AppendBlobReader/src/consumer.js
@@ -416,8 +416,7 @@ function sendDataToSumoUsingSplitHandler(context, dataBytesBuffer, sendOptions, 
                 context.log(`No chunks to send ${serviceBusTask.rowKey}`);
             } else if (numChunksSent === dataChunks.length) {
                 if (numChunksSent === dataChunks.length) {
-                    context.log(`All chunks successfully sent to sumo.`);
-                    context.log(`Blob: ${serviceBusTask.rowKey}, Prefix: ${ignoredprefixLen}, Sent ${dataLenSent} bytes of data. numChunksSent ${numChunksSent}`);
+                    context.log(`All chunks successfully sent to sumo, Blob: ${serviceBusTask.rowKey}, Prefix: ${ignoredprefixLen}, Sent ${dataLenSent} bytes of data. numChunksSent ${numChunksSent}`);
                 }
             }
             resolve(dataLenSent + ignoredprefixLen);
@@ -437,8 +436,7 @@ function errorHandler(err, serviceBusTask, context) {
         discardError = true;
     } else if (err !== undefined && err.statusCode === 416 && err.code === "InvalidRange") {
         // here in case of appendblob data may not exists after startByte
-        context.log(`byte: ${serviceBusTask.startByte} of blob: ${serviceBusTask.rowKey}`);
-        context.log("Offset is already at the end, Exit now!");
+        context.log(`Offset is already at the end, byte: ${serviceBusTask.startByte} of blob: ${serviceBusTask.rowKey}. Exit now!.`);
         discardError = true;
     } else if (err !== undefined && (err.statusCode === 503 || err.statusCode === 500 || err.statusCode == 429)) {
         context.log.verbose("Error in appendBlobStreamMessageHandlerv2 Potential throttling scenario: blob %s %d %d %d %s Exit now!", serviceBusTask.rowKey, serviceBusTask.startByte, serviceBusTask.endByte, err.statusCode, err.code);

a92ca0a759eaba546f4be2db58fe88c77c85980a
Md Azhar <md.azhar@sumologic.com>
2026-04-01 17:32:19 +0530

    Revert "fixing eslint check runs errors"
    This reverts commit 4b691d2d062af48d9ea25d29e865e01ad916aa29.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,10 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "commonjs",
-      globals: {
-        ...globals.node,
-      },
-    },
-    rules: {
-      // keeping lint runnable for legacy codebase
-      "no-undef": "off",
-      "no-unused-vars": "off",
-      "no-redeclare": "off",
-      "no-empty": "off",
-      "no-cond-assign": "off",
-      "no-useless-escape": "off",
-      "no-useless-assignment": "off",
-      "no-prototype-builtins": "off",
-      "no-async-promise-executor": "off",
-    },
-  },
+  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
